50e2442770d6e2b3c4a9255c4ffbeee356271220
lemmark193 <ler.pl@mail.ru>
2022-12-10 05:22:15 +0700

    Добавляет стилизацию кнопки открытия мобильного меню

--- source/js/main.js
+++ source/js/main.js
@@ -3,12 +3,15 @@ const navigationToggle = siteNavigation.querySelector('.site-navigation__toggle'
 const navigationList = siteNavigation.querySelector('.site-navigation__list')
 
 const navigationListHiddenClass = 'site-navigation__list--mobile-hidden';
+const navigationToggleCloseClass = 'site-navigation__toggle--close';
 
 window.addEventListener('load', () => {
   navigationList.classList.add(navigationListHiddenClass);
+  navigationToggle.classList.remove(navigationToggleCloseClass);
   navigationToggle.disabled = false;
 });
 
 navigationToggle.addEventListener('click', () => {
   navigationList.classList.toggle(navigationListHiddenClass);
+  navigationToggle.classList.toggle(navigationToggleCloseClass);
 });
